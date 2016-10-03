9d1dfcf842192a3f6db66626f8a6c0baf57a9a63
juliombb <juliogx@hotmail.com>
2016-10-03 10:34:04 -0300

    Configuração

--- ProjetoMovile/src/api/APIRest.java
+++ ProjetoMovile/src/api/APIRest.java
@@ -12,9 +12,33 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import com.google.gson.Gson;
 
 @Path("/api")
 public class APIRest {
+	@GET
+	@Path("/query/1/{day}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public Response BaseAtivaOperadoraDia(@PathParam("day") String dia){
+		String objEx = "";
+		return Response.status(Status.OK).entity(new Gson().toJson(objEx)).build();
+	}
 	
+	@GET
+	@Path("/query/2")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public Response CancelamentoOperadoraDia(){
+		String objEx = "";
+		return Response.status(Status.OK).entity(new Gson().toJson(objEx)).build();
+	}
+	
+	@GET
+	@Path("/query/3")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public Response ARPUOperadoraDia(){
+		String objEx = "";
+		return Response.status(Status.OK).entity(new Gson().toJson(objEx)).build();
+	}
 }
